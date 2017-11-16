004f0eb42f9c87e0c140dbd2be8e863dae286799
iL1nk <ilink@ua.fm>
2017-11-16 02:14:08 +0200

    WeaterApp: started

---
+++ src/app/app-weather-forecast/app-weather-forecast.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './app-weather-forecast.component.html',
+  styleUrls: ['./app-weather-forecast.component.css']
+})
+export class AppWeatherForecastComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { MatButtonModule, MatFormFieldModule, MatCardModule, MatIconModule, MatInputModule, MatTabsModule, MatProgressSpinnerModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppWeatherForecastComponent } from './app-weather-forecast/app-weather-forecast.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppWeatherForecastComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatFormFieldModule, MatCardModule, MatIconModule, MatInputModule, MatTabsModule, MatProgressSpinnerModule, MatToolbarModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
